b0a1c6188f33046afb0e9227ccc5f79607813def
Deepak-DKJ <deepakjha1104@gmail.com>
2024-12-09 20:08:21 +0530

    Google login button width

--- src/Components/GoogleLoginButton.js
+++ src/Components/GoogleLoginButton.js
@@ -80,6 +80,7 @@ const GoogleLoginButton = () => {
       />
             <GoogleLogin
                 theme='filled_blue'
+                width={20}
                 onSuccess={handleGoogleLogin}
                 onError={() => setError('Google login failed')}
             />
